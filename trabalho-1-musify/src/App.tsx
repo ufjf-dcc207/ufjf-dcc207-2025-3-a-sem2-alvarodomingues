ba0cad89a1930d5dd0496a55fe981362966b1162
alvaroiceufjf <alvaro.domingues@estudante.ufjf.br>
2025-12-09 04:11:11 -0300

    Altera App.tsx para ter controle de estado

--- trabalho-1-musify/src/App.tsx
+++ trabalho-1-musify/src/App.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
-import MusicPlayer from './components/MusicPlayer'; 
+import MusicPlayer from './components/MusicPlayer';
 import HomeView from './views/HomeView';
+import { currentTrack as defaultTrack } from './data';
+import { CurrentTrack } from './types';
 
 const App = () => {
+  // ESTADO 1: A música que está tocando agora
+  // Música que veio do data.ts
+  const [playingTrack, setPlayingTrack] = useState<CurrentTrack>(defaultTrack);
+
+  // Função para trocar a música (passada para a HomeView)
+  const handlePlayMusic = (playlistName: string, image: string) => {
+    setPlayingTrack({
+      titulo: playlistName, // Simplificação: usando nome da playlist como música
+      artista: "Artista Variado",
+      album: "Album Selecionado",
+      capaUrl: image
+    });
+  };
+
   return (
     <div className="spotify-layout">
       <Sidebar />
+      
       <div className="main-content">
-        <HomeView />
+        {/* Passamos a função de trocar música para a Home */}
+        <HomeView onPlay={handlePlayMusic} />
       </div>
-      <MusicPlayer />
+      
+      {/* Passamos o ESTADO da música atual para o Player desenhar */}
+      <MusicPlayer track={playingTrack} />
     </div>
   );
 };
